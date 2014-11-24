ace837d15b0e1cedc9b3d0c2408d8df7580261b1
jp-ca <jp_ca@ymail.com>
2014-11-23 22:41:57 -0800

    added file start.sh

---
+++ start.sh
@@ -0,0 +1,15 @@
+    
+    DEBUG=del ./bin/www
+
+
+
+
+
+#!/bin/sh
+
+# if [ $(ps aux | grep $USER | grep node | grep -v grep | wc -l | tr -s "\n") -eq 0 ]
+# then
+        # export NODE_ENV=production
+        # export PATH=/usr/local/bin:$PATH
+        # DEBUG=del ./bin/www
+# fi
