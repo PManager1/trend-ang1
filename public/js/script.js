228cafe19100edf724f94d60c6c27e4dd041965d
jp-ca <jp_ca@ymail.com>
2014-11-29 14:16:50 -0800

    commented the script file

--- public/js/script.js
+++ public/js/script.js
@@ -1,82 +1,82 @@
-jQuery(document).ready(function($) {
-    // Code that uses jQuery's $ can follow here.
+// jQuery(document).ready(function($) {
+//     // Code that uses jQuery's $ can follow here.
 
 
 
-    // console.log(' document loaded & now script loaded   ==== ', $);
+//     // console.log(' document loaded & now script loaded   ==== ', $);
 
-    // console.log(' search input  = ', $('#searchInput'));
-
-
-    // $('#searchInput').css('background-color', 'black');
-    // console.log(' blacking ~~~~~~~~~~~');
+//     // console.log(' search input  = ', $('#searchInput'));
 
 
-    $(".trenda").on("click", function(event) {
-        event.preventDefault();
-        console.log('clicked  trend a ', $(".trenda"));
-        alert(' a clicked ');
-        // alert ( $( this ).text() );
-    });
+//     // $('#searchInput').css('background-color', 'black');
+//     // console.log(' blacking ~~~~~~~~~~~');
 
 
-
-    // $(".trendli").click(function  (event) {
-    //     event.preventDefault(); 
-    //     console.log( 'link clicked ');
-    //     alert(' a clicked ');     
-    // }); 
+//     $(".trenda").on("click", function(event) {
+//         event.preventDefault();
+//         console.log('clicked  trend a ', $(".trenda"));
+//         alert(' a clicked ');
+//         // alert ( $( this ).text() );
+//     });
 
 
 
-
-    // $('#searchInput').css('background-color','black');  
-
-    // jQuery(selector).click(function(){
-
-
-
-        //  $('.nobullets').on("click", ".trendli", function(e) {
-
-        //     console.log(' From Script js  ');
-
-        //     // $('#searchInput').text('background-color', 'blue');
-        //     $('#searchInput').val(text);
-
-        //     $scope.$apply(function() {
-        //         console.log(' 2  From Script js');
-
-        //     });
-        // });
+//     // $(".trendli").click(function  (event) {
+//     //     event.preventDefault(); 
+//     //     console.log( 'link clicked ');
+//     //     alert(' a clicked ');     
+//     // }); 
 
 
 
 
+//     // $('#searchInput').css('background-color','black');  
 
-
-    // jQuery('.trendli').on('click', function(event) {
-    //     $scope.$apply(function() {
-    //         event.preventDefault();
-    //         console.log(' clicking li');
-    //     });
-    // });
+//     // jQuery(selector).click(function(){
 
 
 
+//         //  $('.nobullets').on("click", ".trendli", function(e) {
 
-    $(window).load(function() {
-        // console.log('not clicked  trend li ', $(".trendli"));
-        // console.log('not clicked  trend a ', $(".trenda"));
+//         //     console.log(' From Script js  ');
 
-    });
+//         //     // $('#searchInput').text('background-color', 'blue');
+//         //     $('#searchInput').val(text);
+
+//         //     $scope.$apply(function() {
+//         //         console.log(' 2  From Script js');
+
+//         //     });
+//         // });
 
 
 
 
 
-    $('#searchInput');
-    // console.log("ready!");
-    // console.log(' $ = ', $);
+
+//     // jQuery('.trendli').on('click', function(event) {
+//     //     $scope.$apply(function() {
+//     //         event.preventDefault();
+//     //         console.log(' clicking li');
+//     //     });
+//     // });
 
 
-});
+
+
+//     $(window).load(function() {
+//         // console.log('not clicked  trend li ', $(".trendli"));
+//         // console.log('not clicked  trend a ', $(".trenda"));
+
+//     });
+
+
+
+
+
+//     $('#searchInput');
+//     // console.log("ready!");
+//     // console.log(' $ = ', $);
+
+
+// });
