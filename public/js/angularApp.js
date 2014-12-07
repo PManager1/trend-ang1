59062ccef354b2cfde2cbfd72ac9d1757e33dc4e
jp-ca <jp_ca@ymail.com>
2014-12-07 10:16:52 -0800

    cleaning angApp.js file

--- public/js/angularApp.js
+++ public/js/angularApp.js
@@ -9,8 +9,6 @@ var app = angular.module('potatoNews', ['underscore']);
 
 
 
-
-
 var potatoNews = angular.module('potatoNews', ['ui.router'])
 
 potatoNews.config(function($sceDelegateProvider) {
@@ -28,8 +26,6 @@ potatoNews.config([
     '$locationProvider',
     function($stateProvider, $urlRouterProvider, $locationProvider) {
 
-
-        
 
         $stateProvider
             .state('trend', { // */*/*/*/   new 
@@ -95,7 +91,7 @@ potatoNews.config([
 
 
 
-//refresh the view 
+// end refresh the view 
 
 
 
@@ -157,6 +153,12 @@ potatoNews.config([
 
 
 
+
+// id controller
+
+
+
+
 //  insta factory 
 
 // yt FACTORY 
@@ -201,20 +203,7 @@ potatoNews.controller('TrendCtrl', ['$scope', 'trends',
     function($scope, trends) {
 
         $scope.trends = trends.trends;
-        $scope.title = '';
-
-        $scope.contacts = [{
-            id: 0,
-            name: "Alice"
-        }, {
-            id: 1,
-            name: "Bob"
-        }, {
-            id: 2,
-            name: "Alice 2"
-        }];
-
-        console.log(' $scope contacts = ', $scope.contacts);
+        
 
         $('.nobullets').on("click", ".trendli", function(e) {
             var text = ($(this).text());
@@ -323,81 +312,9 @@ potatoNews.factory('trends', ['$http', function($http) { // new trend factory
 
 
 
-// MAIN / POSTS  CONTROLLER
-
-potatoNews.controller('MainCtrl', [
-    '$scope',
-    'posts',
-    function($scope, posts) {
-
-        $scope.posts = posts.posts;
-        //setting title to blank here to prevent empty posts
-        $scope.title = '';
-
-        $scope.addPost = function() {
-            if ($scope.title === '') {
-                return;
-            }
-            posts.create({
-                title: $scope.title,
-                link: $scope.link,
-            });
-            //clear the values
-            $scope.title = '';
-            $scope.link = '';
-        };
-
-        $scope.upvote = function(post) {
-            //our post factory has an upvote() function in it
-            //we're just calling this using the post we have
-            console.log(' calling upvote for post');
-            posts.upvote(post);
-        }
-        $scope.downvote = function(post) {
-            posts.downvote(post);
-        };
-
-    }
-]);
 
 
-// COMMENTS  CONTROLLER 
 
-potatoNews.controller('PostsCtrl', [
-    '$scope',
-    'posts',
-    'post',
-    function($scope, posts, post) {
-        //used to need $stateRouterProvider to figure out what
-        //specific post we're grabbing.  Since we used the resolve object to
-        //refer to the posts.get() function and assigned it to the post value
-        //then injected that here, we now have the specific post from the db
-        //we also inject 'posts' so we can screw with the comments
-        $scope.post = post;
-
-        $scope.addComment = function() {
-            if ($scope.body === '') {
-                return;
-            }
-            posts.addComment(post._id, {
-                body: $scope.body,
-                author: 'user',
-            }).success(function(comment) {
-                $scope.post.comments.push(comment);
-            });
-            $scope.body = '';
-        };
-
-        $scope.upvote = function(comment) {
-            posts.upvoteComment(post, comment);
-        };
-
-        $scope.downvote = function(comment) {
-            posts.downvoteComment(post, comment);
-        };
-
-    }
-]);
 
 
 // Underscore FACTORY 
@@ -411,77 +328,4 @@ potatoNews.factory('_', ['$http', function($http) { // new trend factory
 
 
 
-// POSTS FACTORY 
 
-potatoNews.factory('posts', ['$http', function($http) {
-    var o = {
-        posts: []
-    };
-    //query the '/posts' route and, with .success(),
-    //bind a function for when that request returns
-    //the posts route returns a list, so we just copy that into the
-    //client side posts object
-    //using angular.copy() makes ui update properly
-    o.getAll = function() {
-        return $http.get('/posts').success(function(data) {
-            angular.copy(data, o.posts);
-        });
-    };
-    //now we'll need to create new posts
-    //uses the router.post in index.js to post a new Post mongoose model to mongodb
-    //when $http gets a success back, it adds this post to the posts object in
-    //this local factory, so the mongodb and angular data is the same
-    //sweet!
-    o.create = function(post) {
-        return $http.post('/posts', post).success(function(data) {
-            o.posts.push(data);
-        });
-    };
-    //upvotes
-    o.upvote = function(post) {
-        //use the express route for this post's id to add an upvote to it in the mongo model
-        return $http.put('/posts/' + post._id + '/upvote')
-            .success(function(data) {
-                //if we know it worked on the backend, update frontend
-                console.log(' inside o update posts ');
-                post.votes += 1;
-            });
-    };
-    //downvotes
-    o.downvote = function(post) {
-        return $http.put('/posts/' + post._id + '/downvote')
-            .success(function(data) {
-                post.votes -= 1;
-            });
-    };
-    //grab a single post from the server
-    o.get = function(id) {
-        //use the express route to grab this post and return the response
-        //from that route, which is a json of the post data
-        //.then is a promise, a kind of newly native thing in JS that upon cursory research
-        //looks friggin sweet; TODO Learn to use them like a boss.  First, this.
-        return $http.get('/posts/' + id).then(function(res) {
-            return res.data;
-        });
-    };
-    //comments, once again using express
-    o.addComment = function(id, comment) {
-        return $http.post('/posts/' + id + '/comments', comment);
-    };
-    //upvote comments
-    o.upvoteComment = function(post, comment) {
-        return $http.put('/posts/' + post._id + '/comments/' + comment._id + '/upvote')
-            .success(function(data) {
-                comment.votes += 1;
-            });
-    };
-    //downvote comments
-    //I should really consolidate these into one voteHandler function
-    o.downvoteComment = function(post, comment) {
-        return $http.put('/posts/' + post._id + '/comments/' + comment._id + '/downvote')
-            .success(function(data) {
-                comment.votes -= 1;
-            });
-    };
-    return o;
-}]);
