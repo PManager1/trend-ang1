80ad1a006f6503542f426e341eb516abdebb7bcb
jp-ca <jp_ca@ymail.com>
2015-03-25 18:42:04 -0700

    added GA

--- public/js/GA.js
+++ public/js/GA.js
@@ -3,5 +3,5 @@
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
   })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-  ga('create', 'UA-55219744-1', 'auto');
+  ga('create', 'UA-61197561-1', 'auto');
   ga('send', 'pageview');
